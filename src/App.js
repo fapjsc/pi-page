ecd38bd2c8a5283301196017ef24a0fa252f1c8d
fapjsc <p82zsh@gmail.com>
2021-12-01 11:43:55 +0800

    紅利提領點擊後停止auto game

--- src/App.js
+++ src/App.js
@@ -131,6 +131,7 @@ const App = () => {
           <div
             onClick={() => {
               alert('take win');
+              setIsAutoGame(false);
             }}
             className={styles.takeWin}
           />
